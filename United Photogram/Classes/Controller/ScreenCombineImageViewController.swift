72ac7dadf37cc90181881913b80e7d567e5aab9e
koyamaura <k.kindomail@gmail.com>
2016-10-30 12:09:18 +0900

    画像の合成が完了するところまできました

--- United Photogram/Classes/Controller/ScreenCombineImageViewController.swift
+++ United Photogram/Classes/Controller/ScreenCombineImageViewController.swift
@@ -3,13 +3,42 @@ import UIKit
 
 
 
-class ScreenCombineImageViewController: UIViewController {
+class ScreenCombineImageViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
+    @IBOutlet weak var cameraImgView: UIImageView!
+     let stamp = UIImage(named: "dog2.png")
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-}
+
+    @IBAction func btnPushed(sender: AnyObject) {
+        let picker = UIImagePickerController()
+        picker.sourceType = UIImagePickerControllerSourceType.Camera
+        picker.delegate = self
+        presentViewController(picker, animated: true, completion: nil)
+    }
+    
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
+        let stampViews = cameraImgView.subviews
+        for stampView in stampViews {
+            stampView.removeFromSuperview()                     // スタンプをリセットする
+        }
+        cameraImgView.image = image                             // 撮影した画像をセットする
+        cameraImgView.addSubview(UIImageView(image: stamp))     // スタンプ画像を合成する
+
+        // ----- 合成した画像を保存する
+        UIGraphicsBeginImageContext(cameraImgView.bounds.size)
+        cameraImgView.layer.renderInContext(UIGraphicsGetCurrentContext()!)
+        UIImageWriteToSavedPhotosAlbum(UIGraphicsGetImageFromCurrentImageContext()!, self, nil, nil)
+        UIGraphicsEndImageContext()
+        
+        dismissViewControllerAnimated(true, completion: nil)    // アプリ画面へ戻る
+    }
+    
+    }
